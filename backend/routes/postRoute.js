f02e89f44066d25615791042d7f95527c9e44c46
RisvanBCA2022 <111649025+RisvanBCA2022@users.noreply.github.com>
2024-04-20 16:30:21 +0530

    delete post route and controller created

--- backend/routes/postRoute.js
+++ backend/routes/postRoute.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { verifyToken } from '../utils/verifyUser.js'
-import { createpost, getposts } from '../controllers/postController.js'
+import { createpost, deletepost, getposts } from '../controllers/postController.js'
 
 const router=express.Router()
 
 router.post('/create',verifyToken,createpost)
 router.get('/getposts', getposts)
+router.post('/deletepost/:postId/:userId',verifyToken,deletepost)
 
 export default router
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -9,4 +9,5 @@ router.put('/update/:userId',verifyToken,updateUser)
 router.delete('/delete/:userId',verifyToken,deleteuser)
 router.post('/signout',signout)
 
+
 export default router
