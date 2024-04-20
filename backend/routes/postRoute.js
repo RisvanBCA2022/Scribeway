4db62737d2961d38f069c33f616d89991b7b6a4b
RisvanBCA2022 <111649025+RisvanBCA2022@users.noreply.github.com>
2024-04-20 15:12:37 +0530

    getpost api created with dynamic query

--- backend/routes/postRoute.js
+++ backend/routes/postRoute.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { verifyToken } from '../utils/verifyUser.js'
-import { createpost } from '../controllers/postController.js'
+import { createpost, getposts } from '../controllers/postController.js'
 
 const router=express.Router()
 
 router.post('/create',verifyToken,createpost)
+router.get('/getposts', getposts)
 
 export default router
