b53bb7aa4e25dfdc115aea337c0b5d9968c1512c
RisvanBCA2022 <111649025+RisvanBCA2022@users.noreply.github.com>
2024-04-22 22:03:05 +0530

    Home page design and navbar customzation

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -24,8 +24,8 @@ const Navbar = () => {
         </span>{" "}
       </Link>
       <div>
-      <form className="flex-1 justify-start relative max-[600px]:w-1 max-[500px]:hidden">
-          <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-500 dark:text-gray-400" />
+      <form className="flex-1 justify-start relative max-[600px]:w-1">
+          <SearchIcon className="absolute right-3 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-500 dark:text-gray-400" />
           <Input
             className="pl-10 pr-4 py-2 rounded-md bg-gray-100 dark:bg-gray-900 dark:text-gray-200 focus:outline-none focus:ring-1 focus:ring-gray-500 dark:focus:ring-gray-400 w-full max-w-[200px] md:max-w-[300px]"
             placeholder="Search..."
